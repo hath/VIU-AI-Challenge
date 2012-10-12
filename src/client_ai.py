f9255af5e9a84fbe7329dfc4a483cc144025ba8e
Simon Trigona <simon.trigona@gmail.com>
2012-10-12 15:29:02 -0700

    Added a bit more logic/structure to the communication protocol.

--- src/client_ai.py
+++ src/client_ai.py
@@ -2,3 +2,8 @@ import client
 
 client = client.Client()
 client.start()
+
+print client.get_game_state()
+while client.running:
+    client.send_moves({'moves':[([0,3], [0,4]), ([7,6],[6,5])]})
+    print client.get_game_state()
